1900b9bbc8879c2be1bbe0ebf102ed652a047552
João Diego <jdflorencio@gmail.com>
2020-03-30 17:25:53 -0300

    correçao do relacionamento

--- database/migrations/20200326173422-create-prontuario.js
+++ database/migrations/20200326173422-create-prontuario.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'cidade',
+          model: 'pessoa',
           key: 'id'
         }
       },
